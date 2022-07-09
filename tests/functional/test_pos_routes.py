aaa9c3d3aff6d61e6e5034708402f4bed278dad3
Javier Baez <baezdevs@gmail.com>
2022-07-08 19:57:19 -0500

    Test init

---
+++ tests/functional/test_pos_routes.py
@@ -0,0 +1,16 @@
+from src.openwebpos import open_web_pos
+
+
+def test_home_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    flask_app = open_web_pos()
+
+    # create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('/')
+        assert response.status_code == 200
