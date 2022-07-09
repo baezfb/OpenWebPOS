3ce14fef1e3d88f84e8ab290a8cc79542ccd353d
Javier Baez <baezdevs@gmail.com>
2022-07-08 19:57:43 -0500

    updated user models.py

--- src/openwebpos/blueprints/user/models.py
+++ src/openwebpos/blueprints/user/models.py
@@ -1,12 +1,14 @@
 from collections import OrderedDict
 
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from openwebpos.extensions import db
 from openwebpos.utils.sql import SQLMixin
 
 
 class User(UserMixin, SQLMixin, db.Model):
+    __tablename__ = "user"
     ROLE = OrderedDict([
         ('customer', 'Customer'),
         ('staff', 'Staff'),
@@ -20,9 +22,17 @@ class User(UserMixin, SQLMixin, db.Model):
     password = db.Column(db.String(120), nullable=False, server_default='')
     active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
 
+    def check_password(self, password):
+        return check_password_hash(password, self.password)
+
     def is_active(self):
         """
         Return whether user account is active.
         :return: bool
         """
         return self.active
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = generate_password_hash(password)
