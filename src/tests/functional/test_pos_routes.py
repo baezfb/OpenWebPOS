b6e3b2ec22fbc530093704b9be420292e7df0c3d
Javier Baez <baezdevs@gmail.com>
2022-07-11 17:14:32 -0500

    init test login

--- src/tests/functional/test_pos_routes.py
+++ src/tests/functional/test_pos_routes.py
@@ -1,4 +1,3 @@
-# from src.openwebpos import open_web_pos
 from openwebpos import open_web_pos
 
 
@@ -14,4 +13,19 @@ def test_home_page():
     # create a test client using the Flask application configured for testing
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
+        assert response.status_code == 302
+
+
+def test_login_page():
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/login' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    flask_app = open_web_pos()
+
+    # create a test client using the Flask application configured for testing
+    with flask_app.test_client() as test_client:
+        response = test_client.get('user/login')
         assert response.status_code == 200
