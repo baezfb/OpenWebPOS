6fd5a51d3557739a0cb559b3016b4786609c9569
Javier Baez <baezdevs@gmail.com>
2022-07-15 15:36:48 -0500

    added file utils

--- src/openwebpos/utils/__init__.py
+++ src/openwebpos/utils/__init__.py
@@ -1,6 +1,41 @@
 from secrets import token_urlsafe
 
 
+def get_file_extension(filename: str) -> str:
+    """
+    Get the file extension.
+    filename: str, name of the file.
+    return: str, file extension.
+    """
+    from os import path
+    return path.splitext(filename)[1][1:]
+
+
+def allowed_file(filename: str) -> bool:
+    """
+    Check if the file is allowed.
+    filename: str, name of the file.
+    return: bool, True if the file is allowed.
+    """
+    from flask import current_app
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in current_app.config['ALLOWED_EXTENSIONS']
+
+
+def delete_file(file_path: str, file_name: str):
+    """
+    Delete a file if it exists.
+    file_path: str, path to the file.
+    file_name: str, name of the file.
+    """
+    try:
+        from os import path
+        from os import remove
+        remove(path.join(file_path, file_name))
+    except FileNotFoundError:
+        pass
+
+
 def gen_urlsafe_token(length: int) -> str:
     """
     Generate a URL-safe, Base64-encoded securely generated random string.
