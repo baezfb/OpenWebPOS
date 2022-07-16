58f308fe8b476e16115fdd314e8570162fb96cb9
Javier Baez <baezdevs@gmail.com>
2022-07-15 19:41:02 -0500

    refactor pos models.py

--- src/openwebpos/blueprints/pos/models.py
+++ src/openwebpos/blueprints/pos/models.py
@@ -21,10 +21,7 @@ class Category(SQLMixin, db.Model):
         """
         Add categories to the database.
         """
-        category = Category()
-        category.name = 'Food'
-        category.slug = slugify(text='Food')
-        category.description = 'Category description'
+        category = Category(name='Category 1', description='Category Description')
         category.save()
 
     def is_active(self):
@@ -45,7 +42,7 @@ class Item(SQLMixin, db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     slug = db.Column(db.String(50), nullable=False, unique=True)
     description = db.Column(db.String(255))
-    image = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255))
     price = db.Column(db.Float, nullable=False)
     active = db.Column(db.Boolean, default=True)
 
@@ -59,12 +56,7 @@ class Item(SQLMixin, db.Model):
         """
         Add items to the database.
         """
-        item = Item()
-        item.name = 'Burger'
-        item.slug = slugify(text='Burger')
-        item.description = 'Item description'
-        item.price = 10.50
-        item.category_id = Category.query.filter_by(name='Food').first().id
+        item = Item(name='Burger', description='Burger description', price=10.00, category_id=1)
         item.addons = Addon.query.filter_by(item_id=item.id, active=True).all()
         item.options = Option.query.filter_by(item_id=item.id, active=True).all()
         item.save()
@@ -78,8 +70,8 @@ class Item(SQLMixin, db.Model):
     def __init__(self, **kwargs):
         super(Item, self).__init__(**kwargs)
 
-        if self.image is None:
-            self.image = 'placeholder.png'
+        if self.slug is None:
+            self.slug = slugify(text=self.name)
 
         if self.price is None:
             self.price = 0.00
@@ -90,7 +82,7 @@ class Option(SQLMixin, db.Model):
 
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255))
-    image = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255))
     price = db.Column(db.Float, nullable=False)
     active = db.Column(db.Boolean, default=True)
 
@@ -102,11 +94,7 @@ class Option(SQLMixin, db.Model):
         """
         Add options to the database.
         """
-        option = Option()
-        option.name = 'Option1'
-        option.description = 'Option1 Description'
-        option.price = 10.00
-        option.item_id = Item.query.filter_by(name='Burger').first().id
+        option = Option(name='Option 1', description='Option Description', price=10.00, item_id=1)
         option.save()
 
     def is_active(self):
@@ -114,8 +102,6 @@ class Option(SQLMixin, db.Model):
 
     def __init__(self, **kwargs):
         super(Option, self).__init__(**kwargs)
-        if self.image is None:
-            self.image = 'placeholder.png'
 
         if self.price is None:
             self.price = 0.00
@@ -126,7 +112,7 @@ class Addon(SQLMixin, db.Model):
 
     name = db.Column(db.String(50), nullable=False)
     description = db.Column(db.String(255))
-    image = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255))
     price = db.Column(db.Float, nullable=False)
     active = db.Column(db.Boolean, default=True)
 
@@ -138,11 +124,7 @@ class Addon(SQLMixin, db.Model):
         """
         Add addons to the database.
         """
-        addon = Addon()
-        addon.name = 'Addon1'
-        addon.description = 'Addon1 Description'
-        addon.price = 10.00
-        addon.item_id = Item.query.filter_by(name='Burger').first().id
+        addon = Addon(name='Addon 1', description='Addon Description', price=10.00, item_id=1)
         addon.save()
 
     def is_active(self):
@@ -150,8 +132,6 @@ class Addon(SQLMixin, db.Model):
 
     def __init__(self, **kwargs):
         super(Addon, self).__init__(**kwargs)
-        if self.image is None:
-            self.image = 'placeholder.png'
 
         if self.price is None:
             self.price = 0.00
