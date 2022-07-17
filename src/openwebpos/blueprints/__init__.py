fd427fd219415761c89fa5e69e3d9bd213ef73f4
Javier Baez <baezdevs@gmail.com>
2022-07-16 20:12:02 -0500

    refactor user routes

--- src/openwebpos/blueprints/__init__.py
+++ src/openwebpos/blueprints/__init__.py
@@ -1,6 +1,6 @@
 from .admin import bp as admin_bp
 from .pos import bp as pos_bp
-from .user import bp as user_bp
+from .user import user as user_bp
 
 blueprints = [
     user_bp,
--- src/openwebpos/blueprints/user/__init__.py
+++ src/openwebpos/blueprints/user/__init__.py
@@ -1,15 +1,26 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 
+from .decorators import staff_required
 from .forms import StaffLoginForm
-from .models import User, Role
+from .models import User
 
-bp = Blueprint('user', __name__, template_folder='templates', url_prefix='/user/')
+user = Blueprint('user', __name__, template_folder='templates', url_prefix='/user/')
 
 
-@bp.get('/login')
+@user.before_request
+def before_request():
+    pass
+
+
+@user.get('/login')
 def login():
+    """
+    Login page.
+    """
+    if User.query.first() is None:
+        return redirect(url_for('core.setup'))
     # redirect to the index page if the user is already logged in
     if current_user.is_authenticated:
         return redirect(url_for('pos.index'))
@@ -17,32 +28,33 @@ def login():
     return render_template('user/login.html', form=form)
 
 
-@bp.post('/login')
+@user.post('/login')
 def login_post():
+    """
+    Handle the POST request for the login form.
+    """
     form = StaffLoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(pin=form.pin.data).first_or_404()
+        usr = User.query.filter_by(pin_id=form.pin.data).first()
 
-        # check if the user exists
-        if user is None:
-            flash('Invalid pin')
-            return redirect(url_for('user.login'))
-
-        # Login the user if its active
-        if login_user(user, remember=False) and user.is_active():
-            user.update_activity_tracking(request.remote_addr)
+        if usr and usr.is_active:
+            login_user(usr)
+            flash('You are logged in.', 'success')
             next_page = request.args.get('next')
             if not next_page or url_parse(next_page).netloc != '':
                 next_page = url_for('pos.index')
-            flash('You are now logged in')
+                return redirect(next_page)
             return redirect(next_page)
-        return redirect(url_for('pos.index'))
     return redirect(url_for('user.login'))
 
 
-@bp.get('/logout')
+@user.get('/logout')
 @login_required
 def logout():
+    """
+    Handle the logout request.
+    """
+    # Logout the user.
     logout_user()
     flash('You have been logged out.', 'success')
     return redirect(url_for('pos.index'))
